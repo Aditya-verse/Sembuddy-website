9f592590e54772b8fa52729a3e9b85cb8dc8584a
Aditya-verse <adityamane5424@gmail.com>
2026-03-15 17:56:11 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,54 +1,32 @@
-<<<<<<< HEAD
-import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig(({ mode }) => {
 
-  const env = loadEnv(mode, process.cwd(), '')
+  const env = loadEnv(mode, process.cwd(), "")
 
   return {
+    base: "/",
 
-    // Correct for Firebase hosting
-    base: '/',
+    server: {
+      port: 3000,
+      host: "0.0.0.0"
+    },
 
     plugins: [react()],
 
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || "no-key-found"),
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || "no-key-found")
+      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY || "no-key-found"),
+      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY || "no-key-found")
     },
 
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './')
+        "@": path.resolve(__dirname, "./")
       }
     }
 
   }
-})
-=======
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
->>>>>>> 963564cf257c0260b367bed0e719edb9155624e2
+})
